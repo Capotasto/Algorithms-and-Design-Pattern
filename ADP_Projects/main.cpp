f11b3b88f3eb9c5f0ab01749817b7533b98d9ce0
Capotasto <rocker519804@gmail.com>
2016-04-09 13:34:51 -0700

    Initial Commit

---
+++ ADP_Projects/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ADP_Projects
+//
+//  Created by Norio Egi on 4/9/16.
+//  Copyright © 2016 Capotasto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
